d0929e615410883d9dca8211ff1258c93ed84678
Yana~ <sharik.spartak2@mail.ru>
2023-02-17 16:08:18 +0300

    update

--- main.js
+++ main.js
@@ -1,63 +1,62 @@
 const showButton = document.querySelector('.show-more__button');
+const hiddenParts = document.querySelector('.list__hidden');
 const showLess = document.querySelector('.show-more__icon');
-const showMore = document.querySelector('.show-more');
-const itemsCount = document.querySelectorAll('.card').length;
-const cards = document.querySelectorAll('.swiper-wrapper > .card');
-const wrap = document.querySelector('.swiper-wrapper');
+
+let div1 = document.createElement('div');
+let div2 = document.createElement('div');
+div1.setAttribute('class', 'swiper-slide card');
+div2.setAttribute('class', 'swiper-slide card');
+showButton.addEventListener('click', function() {
+    hiddenParts.classList.toggle('list__hidden');
+    showLess.classList.toggle('show-more__icon');
+    div1.innerHTML = `
+        <img class="card__logo" src="./img/samsung.svg" alt="Samsung">
+        <img src="./img/go.svg" alt="logo" class="card__go">
+        `
+        div2.innerHTML = `
+        <img class="card__logo" src="./img/ViewSonic.svg" alt="ViewSonic">
+        <img src="./img/go.svg" alt="logo" class="card__go">
+        `
+    document.getElementById('slide').appendChild(div1);
+    document.getElementById('slide').appendChild(div2);
+    if(hiddenParts.classList.contains('list__hidden')) {
+        showButton.textContent = 'Показать всё';
+    } else {
+        showButton.textContent = 'Скрыть';
+    }
+});
+
 const slider = document.querySelector('.swiper');
 
-
-let htmlCard = document.createElement('div');
-
-showMore.addEventListener('click', () => {
-    showLess.classList.toggle('show-more__icon');
-    showButton.className+='hide-btn';
-    slider.classList.toggle('go-down');
-    const arr = Array.from(document.querySelectorAll('.swiper-wrapper > .card'));
-    arr.forEach(el => el.classList.toggle('is-visiable'));
-
-    if(showButton.classList.contains(' hide-btn')){
-        showButton.textContent = 'Скрыть';
-    } else {
-        showButton.textContent = 'Показать всё';
-    }
-})
-if(window.innerWidth >= 768){
-    console.log('h')
-    htmlCard.className = 'last-card hidden-el card card--position hide-card';
-    htmlCard.innerHTML = '<img src="./img/apple.svg" alt="logo" class="card__logo"><img src="./img/go.svg" alt="logo" class="card__go">'
-    wrap.appendChild(htmlCard);
-    for(let i = 6; i < cards.length; i++){
-        cards[i].classList.add('hide-card');
-    }
-
-}
-
 let mySwiper;
-function mobileSlide(){
-    if(window.innerWidth <= 767 && slider.dataset.mobile == 'false'){
+
+function mobileSlider() {
+    if (window.innerWidth <= 767 && slider.dataset.mobile == 'false') {
         mySwiper =  new Swiper(slider, {
-                slidesPerView: 1,
-                spaceBetween: 16,
-                width: 240,
-                height: 72,
-            pagination: {   
-                spaceBetween: 16,
-                loop: true,
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
+            slidesPerView: 1,
+            spaceBetween: 16,
+            width: 240,
+            height: 72,
+            slideClass: 'card',
+        pagination: {   
+            spaceBetween: 16,
+            loop: true,
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
         slider.dataset.mobile = 'true';
     }
-    if(window.innerWidth >= 768 ){
+
+    if (window.innerWidth >= 768) {
         slider.dataset.mobile = 'false';
-        mySwiper.destroy();
+            mySwiper.destroy();
+
     }
 }
 
-mobileSlide();
+mobileSlider();
 
 window.addEventListener('resize', () => {
-    mobileSlide();
+    mobileSlider();
 })
